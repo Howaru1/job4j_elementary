2ad2ec0899dbb4997fc35641f138874ad9768c3b
Howaru1 <Hellwudpecker666@ya.ru>
2023-07-11 21:21:05 +0300

    6.4.0. Слово начинается с ... [#41585  [#41585 #428664]]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -5,8 +5,8 @@ public class ArrayChar {
         boolean result = true;
         for (int i = 0; i < pref.length; i++) {
             if (word[i] != pref[i]) {
-            result = false;
-            break;
+                result = false;
+                break;
             }
         }
         return result;
